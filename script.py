7b3bae36715dcc606415b1a21560a1498c166096
bix2702 <58595172+bix2702@users.noreply.github.com>
2019-12-13 09:11:48 +0100

    Update script.py

--- script.py
+++ script.py
@@ -3,27 +3,35 @@ from gpiozero import CPUTemperature
 from rpi_ws281x import *
 from time import sleep
 
-# Init LED stripe
+# setup pin and number of LEDs
 TOTAL_LED_COUNT = 5
 LED_PIN = 18
-strip = Adafruit_NeoPixel(TOTAL_LED_COUNT, LED_PIN)
+
+# setup NeoPixel Leds (Alternative: Adafruit_NeoPixel( ...) )
+strip = PixelStrip(TOTAL_LED_COUNT, LED_PIN)
 
 # readout of ‘/sys/class/thermal/thermal_zone0/temp’
 cpu = CPUTemperature()
 
+# start invinite loop
 while True:
 	temp = cpu.temperature
 	print(‘CPU temp: {}°C’.format(temp))
+	
 	strip.begin()
+	for led in range(5):
+		strip.setPixelColor(led, Color(0, 0, 0))
+
 	if temp > 40.0:
-		strip.setPixelColorRGB(0, 0, 255, 0)
+		strip.setPixelColor(0, Color(0, 255, 0))
 	if temp > 50.0:
-		strip.setPixelColorRGB(1, 0, 128, 128)
+		strip.setPixelColor(1, Color(255, 255, 0))
 	if temp > 60.0:
-		strip.setPixelColorRGB(2, 0, 255, 255)
+		strip.setPixelColor(2, Color(255, 128, 0))
 	if temp > 70.0:
-		strip.setPixelColorRGB(3, 128, 255, 0)
+		strip.setPixelColor(3, Color(255, 0, 255))
 	if temp > 80.0:
-		strip.setPixelColorRGB(4, 255, 0, 0)
+		strip.setPixelColor(4, Color(255, 0, 0))
+	
 	strip.show()
 	sleep(0.2)	
